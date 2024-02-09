6292ae8627bb0c741bebe822cf97378cfbbdd200
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-02-09 22:23:08 +0600

    SpringBoot
    Practice web application using java

---
+++ Therap/CrudSpringBootJavaGuides/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package net.javaguides.springboot.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+//import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import net.javaguides.springboot.service.EmployeeService;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeService;
+	
+	//display list of employees
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listEmployees",employeService.getAllEmployees() );
+		return "index";
+	}
+
+}
