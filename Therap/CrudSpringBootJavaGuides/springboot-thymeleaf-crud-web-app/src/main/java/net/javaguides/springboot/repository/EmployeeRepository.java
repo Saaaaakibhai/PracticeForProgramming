133e6599c112be2dce05a34658630390ea924c21
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-02-21 17:33:40 +0600

    Therap

--- Therap/CrudSpringBootJavaGuides/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ Therap/CrudSpringBootJavaGuides/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import net.javaguides.springboot.model.Employee;
+
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 	
--- Therap/CrudSpringBootJavaGuides/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
+++ Therap/CrudSpringBootJavaGuides/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -3,10 +3,13 @@ package net.javaguides.springboot.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
