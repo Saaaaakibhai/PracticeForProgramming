35b4a767ca2f7b1452404141127b6e22260e805f
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-09 14:56:27 +0600

    part 01

---
+++ Therap/therapjavafest/src/basicprogramming/arraymanipulation.java
@@ -0,0 +1,9 @@
+package basicprogramming;
+
+public class arraymanipulation {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        System.out.println("HI Brother"+"Yes I'm G00d");
+
+    }
+}
