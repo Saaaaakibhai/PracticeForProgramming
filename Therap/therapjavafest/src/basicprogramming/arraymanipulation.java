e4dd7004926da0d02c051b1410e6b016ccb9aea2
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-12 18:29:32 +0600

    array manipulation

--- Therap/therapjavafest/src/basicprogramming/arraymanipulation.java
+++ Therap/therapjavafest/src/basicprogramming/arraymanipulation.java
@@ -1,9 +1,33 @@
 package basicprogramming;
 
-public class arraymanipulation {
-    public static void main(String[]args){
-        System.out.println("Hello World");
-        System.out.println("HI Brother"+"Yes I'm G00d");
+import java.util.Scanner;
 
+public class arraymanipulation {
+    public static void main(String[] args) {
+        int q,n;
+        Scanner input=new Scanner(System.in);
+        //Queries = rows
+        //N = Columns
+
+        n = input.nextInt();
+        q = input.nextInt();
+        int queries [][] = new int [n][q];
+        long arr []= new long[n+2];
+
+        for (int i=0;i<queries.length;i++){
+            int a = queries[i][0];
+            int b = queries[i][1];
+            int k = queries[i][2];
+
+            arr[a] = arr[a]+k;
+            arr[b+1]= arr[b+1]-k;
+        }
+        long max = 0;
+        for (int i = 1; i<=n;i++){
+            arr[i]= arr[i]+arr[i-1];
+            max = Math.max(arr[i],max);
+        }
+        return max;
     }
+
 }
